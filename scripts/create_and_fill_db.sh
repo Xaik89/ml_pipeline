14c95717abe150568946b1feefff7414a6b7e12d
Andrey Khaykin <andrey.khaykin@edgify.ai>
2021-07-14 17:31:41 +0400

    fix to relative path

--- scripts/create_and_fill_db.sh
+++ scripts/create_and_fill_db.sh
@@ -4,11 +4,11 @@
 awslocal dynamodb create-table --cli-input-json file://scripts/table-definition.json
 
 # fill DB
-python scripts/data/load_metadata.py --meta ../dataset/styles.csv --images ../dataset/images/
+python scripts/load_metadata.py --meta dataset/styles.csv --images dataset/images/
 
 # create and upload to S3
 awslocal s3 mb "s3://static-s3-bucket"
 
 awslocal s3 \
-  sync ../dataset/images/ \
+  sync dataset/images/ \
  "s3://static-s3-bucket/images/"
