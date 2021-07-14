99bd96620d40e6c81731887f68a4382481139ed1
Andrey Khaykin <andrey.khaykin@edgify.ai>
2021-07-14 16:39:39 +0400

    disable PRO version, re-run notebook

--- localstack/bin/localstack-entrypoint.sh
+++ localstack/bin/localstack-entrypoint.sh
@@ -7,19 +7,3 @@ aws configure set aws_secret_access_key test
 echo "[default]" > ~/.aws/config
 echo "region = us-east-1" >> ~/.aws/config
 echo "output = json" >> ~/.aws/config
-
-
-printf "Configuring localstack components..."
-
-# create and upload to S3
-# awslocal s3 mb "s3://static-s3-bucket"
-
-# # lambda + sqs
-# awslocal lambda create-function --function-name s3-copy-lambda --code ImageUri=s3-copy-lambda:latest --role arn:aws:iam::000000000:role/lambda-ex
-
-# awslocal sqs create-queue --queue-name s3-queue
-
-# # bound to lambdas
-# awslocal lambda create-event-source-mapping \
-#  --function-name s3-copy-lambda --batch-size 1 \
-#  --event-source-arn arn:aws:sqs:us-east-1:000000000000:s3-queue
